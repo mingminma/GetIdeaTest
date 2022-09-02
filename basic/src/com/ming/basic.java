8a64de87f4f9db0c2d19ab4c6ef1d02cd079b248
mamingmin <1712832576@qq.com>
2022-09-02 22:45:47 +0800

    测试代码提交

---
+++ basic/src/com/ming/basic.java
@@ -0,0 +1,14 @@
+package com.ming;
+
+/**
+ * @ClassName basic
+ * @Description TODO
+ * @Author ming
+ * @Date 2022/09/02 22:44
+ * @Version 1.0
+ **/
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
